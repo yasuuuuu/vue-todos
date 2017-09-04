fda12a23656df3ec5c38596288f8c6460ece19ae
yujikudo <yuji.kudo@gmail.com>
2017-09-04 14:30:35 +0900

    コンポーネントも使わない

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  Vue.component('todo', {
-    template: '\
-    <li v-bind:class="{ completed: todo.done }">\
-      <input type="checkbox" v-model="todo.done">{{ todo.text }}\
-      <input type="button" v-on:click="deleteTodo" value="削除">\
-    </li>',
-    props: {
-      todo: Object
-    },
-    methods: {
-      deleteTodo: function(){
-        this.$emit('delete-todo', this.todo.id);
-      }
-    }
-  })
-
   var vm = new Vue({
     el: '#app',
     data: { 

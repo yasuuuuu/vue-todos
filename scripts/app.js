fafba10e2df96487e61d0258f7917e26d339640b
yujikudo <yuji.kudo@gmail.com>
2017-09-04 14:18:55 +0900

    ステージ2

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
 
   Vue.component('todo', {
-    template: '<li>{{ todo.text }}</li>',
+    template: '\
+    <li v-bind:class="{ completed: todo.done }">\
+      <input type="checkbox" v-model="todo.done">{{ todo.text }}\
+      <input type="button" v-on:click="deleteTodo" value="削除">\
+    </li>',
     props: {
       todo: Object
+    },
+    methods: {
+      deleteTodo: function(){
+        this.$emit('delete-todo', this.todo.id);
+      }
     }
   })
 
@@ -11,17 +20,29 @@ document.addEventListener("DOMContentLoaded", function(){
     el: '#app',
     data: { 
       todos: [
-        { text: 'hogehoge' },
-        { text: 'hogehoge2' },
+        { id: 0, text: '合いびき肉を買ってくる', done: false },
+        { id: 1, text: '胡椒を買ってくる', done: false },
       ],
       newTodoText: ''
     },
     methods: {
+      getNewId: function(){
+        return this.todos.length;
+      },
       addTodo: function(){
-        this.todos.push({ text: this.newTodoText })
+        this.todos.push({ id: this.getNewId(), text: this.newTodoText, done: false })
         this.newTodoText = '';
+      },
+      deleteTodo: function(id){
+        for(var i=0; i<this.todos.length;i++){
+          if(this.todos[i].id == id) {
+            this.todos.splice(i, 1);
+            return;
+          }
+        }
       }
     }
+
   })
 
 });

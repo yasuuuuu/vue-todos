400eda54da6e5c500ad175ae4bfdc8be3144934b
Yasuhiro Kume <yasu_figure8@yahoo.co.jp>
2017-09-14 02:55:58 +0900

    component化

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,43 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  const vm = new Vue({
-    el: '#app',
-    data: { 
-      todos: [],
-      newTodoText: ''
+  Vue.component('todo-list-item', {
+    template: '<li v-bind:class="{ completed: todo.done }" v-bind:key="todo.id"><input type="checkbox" v-model="todo.done"> {{ todo.text }} <input type="button" v-on:click="deleteFunc(todo.id)" value="削除"></li>',
+    props: {
+      todo: Object,
+      deleteFunc: Function,
+    },
+  });
+
+  Vue.component('todo-form', {
+    template: '<form v-on:submit.prevent="addTodo"><input type="text" v-model="newTodoText"/><input type="submit" value="書き込み" /></form>',
+    props: {
+      addFunc: Function,
+    },
+    data() {
+      return {
+        newTodoText: '',
+      }
     },
     methods: {
       getNewId() {
         return new Date().getTime().toString(16) + Math.floor(Math.random() * 10000).toString(16);
       },
       addTodo() {
-        this.todos.push({ id: this.getNewId(), text: this.newTodoText, done: false })
+        const newTodo = { id: this.getNewId(), text: this.newTodoText, done: false };
         this.newTodoText = '';
-      },
+        this.$emit('todo-added', newTodo);
+      }
+    }
+  });
+
+  const vm = new Vue({
+    el: '#app',
+    data: { 
+      todos: [],
+    },
+    methods: {
       deleteTodo(id) {
-        for(let i=0; i < this.todos.length; i++){
+        for(let i = 0; i < this.todos.length; i++){
           if(this.todos[i].id === id) {
             this.todos.splice(i, 1);
             return;
@@ -24,5 +46,4 @@ document.addEventListener("DOMContentLoaded", function(){
       }
     }
   })
-
 });

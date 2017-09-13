0bb79eec0ff983283cdec5d0b4b1c58b9d271f59
Yasuhiro Kume <yasu_figure8@yahoo.co.jp>
2017-09-14 02:13:17 +0900

    es2015化

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  var vm = new Vue({
+  const vm = new Vue({
     el: '#app',
     data: { 
       todos: [],
       newTodoText: ''
     },
     methods: {
-      getNewId: function(){
+      getNewId() {
         return new Date().getTime().toString(16) + Math.floor(Math.random() * 10000).toString(16);
       },
-      addTodo: function(){
+      addTodo() {
         this.todos.push({ id: this.getNewId(), text: this.newTodoText, done: false })
         this.newTodoText = '';
       },
-      deleteTodo: function(id){
-        for(var i=0; i<this.todos.length;i++){
-          if(this.todos[i].id == id) {
+      deleteTodo(id) {
+        for(let i=0; i < this.todos.length; i++){
+          if(this.todos[i].id === id) {
             this.todos.splice(i, 1);
             return;
           }
